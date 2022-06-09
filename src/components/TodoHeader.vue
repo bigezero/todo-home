c9a1c2b187c1e4fa3d66b3927a4c74669246037f
bigezero <2415774857@qq.com>
2022-06-09 12:57:29 +0800

    home

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -7,12 +7,29 @@
       class="new-todo"
       placeholder="输入任务名称-回车确认"
       autofocus
+      @keydown.enter="addNew"
+      v-model="addContent"
     />
   </header>
 </template>
 
 <script>
+import eventBus from '../EventBus'
 export default {
- 
+ data(){
+   return{
+     addContent:""
+   }
+ },
+ methods: {
+   addNew(){
+     const addNewContent = this.addContent.trim()
+     if(addNewContent == 0){
+       return
+     }else{
+       eventBus.$emit('send',addNewContent)
+     }
+   }
+ }
 }
 </script>
--- src/components/TodoMain.vue
+++ src/components/TodoMain.vue
@@ -2,9 +2,9 @@
   <ul class="todo-list">
     <!-- completed: 完成的类名 -->
     <li class="completed" >
-      <div class="view">
+      <div class="view" v-for="(item,index) in list" :key="index">
         <input class="toggle" type="checkbox" />
-        <label>任务名</label>
+        <label>{{item}}</label>
         <button class="destroy"></button>
       </div>
     </li>
@@ -13,6 +13,17 @@
 </template>
 
 <script>
+import eventBus from '../EventBus'
 export default {
+  created(){
+    eventBus.$on('send',(newContent)=>{
+      this.list.push(newContent);
+    })
+  },
+  data(){
+    return {
+      list:["eat","codings","sleeping"]
+    }
+  }
 }
 </script>
